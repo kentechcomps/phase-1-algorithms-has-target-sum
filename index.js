d6736162ccf1f73f2078eeb9240bfcb504048a9d
user <mutukukennedy34@gmail.com>
2023-06-29 11:12:51 +0300

    targetsumalgo

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+const numbers = [4 , 5 , 6 ,7]
+const targetVal = 16
+
+hasTargetSum(numbers, targetVal)
+
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    const firstnumberinarray = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      const secodnumberinarray = array[j]; 
+    
+    if((firstnumberinarray + secodnumberinarray)===target){
+      return true
+    } else{
+      return false
+    }
+    
+  }
+}
 }
 
 /* 
   Write the Big O time complexity of your function here
+    The big o complexity here will be Quadratic complexity
+    this is where by Quadratic time: O(n²) (
 */
 
 /* 
   Add your pseudocode here
+  first we will loop through our array to establish the first element to be added
+  Then we will loop the second array using a j value inorder  to obtain the second element in array
+  then you will have to run a check to see if the sum between the i elemnt and the j element equals to the target element
 */
 
 /*
